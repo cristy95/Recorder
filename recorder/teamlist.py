0b544b4c0e1975d048b36d9a4d9ed509a4a4b0e8
cristy95 <cristylorainefuerzas@gmail.com>
2014-12-15 21:01:25 +0800

    added docstring to __init__ and changed tabs to spaces

--- recorder/teamlist.py
+++ recorder/teamlist.py
@@ -1,24 +1,24 @@
 """DOCSTRING"""
 
 class TeamList(object):
-	def __init__(self):
-		self.teams = {}
+    def __init__(self):
+        self.teams = {}
 
-	def add_team(self, team):
-		self.teams[team.name] = team.score
+    def add_team(self, team):
+        self.teams[team.name] = team.score
 
-	def get_score(self, team_name):
-		return self.teams.get(team_name)
+    def get_score(self, team_name):
+        return self.teams.get(team_name)
 
-	def add_score(self, team_name, score):
-		prev_score = self.get_score(team_name)
-		if score is not None:
-			if prev_score is None:
-				self.teams[team_name] = int(score) + 0
-			else:
-				self.teams[team_name] = int(score) + prev_score
-		else:
-			if prev_score is None:
-				self.teams[team_name] = 0 + 0
-			else:
-				self.teams[team_name] = 0 + prev_score
+    def add_score(self, team_name, score):
+        prev_score = self.get_score(team_name)
+        if score is not None:
+            if prev_score is None:
+                self.teams[team_name] = int(score) + 0
+            else:
+                self.teams[team_name] = int(score) + prev_score
+        else:
+            if prev_score is None:
+                self.teams[team_name] = 0 + 0
+            else:
+                self.teams[team_name] = 0 + prev_score
