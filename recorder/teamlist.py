7f7a9aa4634abd07cf3af4142c804683e47bd4a1
cristy95 <cristylorainefuerzas@gmail.com>
2014-12-15 21:07:02 +0800

    added docstrings

--- recorder/teamlist.py
+++ recorder/teamlist.py
@@ -1,16 +1,21 @@
 """DOCSTRING"""
 
 class TeamList(object):
+	"""List for the teams on a particular game"""
     def __init__(self):
+        """Initializing the list"""
         self.teams = {}
 
     def add_team(self, team):
+        """adds a team to the list"""
         self.teams[team.name] = team.score
 
     def get_score(self, team_name):
+        """gets the score of a team"""
         return self.teams.get(team_name)
 
     def add_score(self, team_name, score):
+        """updates the score of a team"""
         prev_score = self.get_score(team_name)
         if score is not None:
             if prev_score is None:
